b3eb6920295b9fa1d3878df47e3eaccc5c607a6e
Riyan <74947287+Ri-yan@users.noreply.github.com>
2024-01-29 22:26:30 +0530

    Delete ConfigureService/ConfigureService/ServiceCollection directory

--- ConfigureService/ConfigureService/ServiceCollection/IServiceInstaller.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MyAPI.ConfigureService.ServiceCollection
-{
-    public interface IServiceInstaller
-    {
-        void Install(IServiceCollection services, IConfiguration configuration);
-    }
-}
