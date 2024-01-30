7db7352083faf86ca44fff4820eb56fac1388bfa
Riyan <74947287+Ri-yan@users.noreply.github.com>
2024-01-30 10:40:14 +0530

    Update CryptoService.cs

--- MyAPI/ConfigureService/ServiceCollection/CryptoService.cs
+++ MyAPI/ConfigureService/ServiceCollection/CryptoService.cs
@@ -5,6 +5,130 @@ namespace MyAPI.ConfigureService.ServiceCollection
 {
     public class CryptoService
     {
+        private const string key = "01234567890123456789012345678901";
+        private const string iv = "0123456789ABCDEF";
+
+        public string Encrypt(string plainText)
+        {
+            byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+            byte[] encryptedBytes;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = Encoding.UTF8.GetBytes(iv);
+                aes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        csEncrypt.Write(plainBytes, 0, plainBytes.Length);
+                        csEncrypt.FlushFinalBlock();
+                    }
+                    encryptedBytes = msEncrypt.ToArray();
+                }
+            }
+
+            return Convert.ToBase64String(encryptedBytes);
+        }
+
+        public string Decrypt(string encryptedText)
+        {
+            byte[] encryptedBytes = Convert.FromBase64String(encryptedText);
+            string decryptedText;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = Encoding.UTF8.GetBytes(iv);
+                aes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                using (var msDecrypt = new MemoryStream(encryptedBytes))
+                {
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            decryptedText = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return decryptedText;
+        }
+
+        public string EncryptForUri(string plainText)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = Encoding.UTF8.GetBytes(iv);
+                aes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+                byte[] encryptedBytes;
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        csEncrypt.Write(plainBytes, 0, plainBytes.Length);
+                        csEncrypt.FlushFinalBlock();
+                        encryptedBytes = msEncrypt.ToArray();
+                    }
+                }
+
+                string base64Encoded = Convert.ToBase64String(encryptedBytes);
+                string urlEncoded = Uri.EscapeDataString(base64Encoded).Replace("/", "_");
+
+                return urlEncoded;
+            }
+        }
+
+        public string DecryptFromUri(string encryptedForUri)
+        {
+            encryptedForUri = encryptedForUri.Replace("_", "/"); // Replace '_' with '/'
+
+            string base64Encoded = Uri.UnescapeDataString(encryptedForUri);
+
+            byte[] encryptedBytes = Convert.FromBase64String(base64Encoded);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = Encoding.UTF8.GetBytes(iv);
+                aes.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                string decryptedText;
+
+                using (MemoryStream msDecrypt = new MemoryStream(encryptedBytes))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            decryptedText = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+                return decryptedText;
+            }
+        }
+
+        /*
         private readonly string secretKey = "MySecretKey123!@#";
 
         public string Encrypt(object data)
@@ -66,6 +190,6 @@ namespace MyAPI.ConfigureService.ServiceCollection
         {
             string encryptedData = Uri.UnescapeDataString(urlEncodedData.Replace("_", "/"));
             return Decrypt(encryptedData);
-        }
+        }*/
     }
 }
